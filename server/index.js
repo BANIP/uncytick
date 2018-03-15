692e8c49b625bd40df180103343174de20bada2c
EC2 Default User <ec2-user@ip-172-31-7-51.ap-northeast-2.compute.internal>
2018-03-15 14:23:57 +0000

    stash

--- server/index.js
+++ server/index.js
@@ -9,23 +9,6 @@ const app = express();
 const server = require("http").createServer(app);
 var socketio = require("./socket")(server);
 
-// [CONFIGURE SECURITY-POLICY]
-app.use(helmet.csp({
-    'default-src': ["'self'"],
-    'connect-src': [
-      "'self'" , "blob:",
-      'wss:',
-      'websocket.domain',
-    ],
-    'font-src': ["'self'",'s3.amazonaws.com',"maxcdn.bootstrapcdn.com"],
-    'img-src': ["'self'", 'data:'],
-    'style-src': ["'self'","maxcdn.bootstrapcdn.com",'s3.amazonaws.com',"'unsafe-inline'"],
-    'script-src': ["'self'","'unsafe-inline'","'unsafe-eval'",'blob:'],
-    reportOnly: false,
-    setAllHeaders: false,
-    safari5: false
-  }))
-  
 // [console]
 console.log("uncytick module start")
 
