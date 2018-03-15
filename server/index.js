ae9e7063ac671432e83b5e5fa642fe558b519057
BANIP <rnaos90@naver.com>
2018-03-15 15:53:43 +0900

    add security

--- server/index.js
+++ server/index.js
@@ -9,10 +9,30 @@ const app = express();
 const server = require("http").createServer(app);
 var socketio = require("./socket")(server);
 
-// [CONFIGURE ROUTER]
-
+// [CONFIGURE SECURITY-POLICY]
+securitySetup = function(app) {
+    var connectSources, helmet, scriptSources, styleSources;
+    helmet = require("helmet");
+    app.use(helmet());
+    app.use(helmet.hidePoweredBy());
+    app.use(helmet.noSniff());
+    app.use(helmet.crossdomain());
+    scriptSources = ["'self'", "'unsafe-inline'", "'unsafe-eval'", "ajax.googleapis.com"];
+    styleSources = ["'self'", "'unsafe-inline'", "ajax.googleapis.com"];
+    connectSources = ["'self'", "ws://localhost:3000"]
+    return app.use(helmet.contentSecurityPolicy({
+      defaultSrc: ["'self'"],
+      scriptSrc: scriptSources,
+      styleSrc: styleSources,
+      connectSrc: connectSources,
+      reportUri: '/report-violation',
+      reportOnly: false,
+      setAllHeaders: false,
+      safari5: false
+    }));
+  };
 // [console]
-console.log("sketch module start")
+console.log("uncytick module start")
 
 module.exports = app;
 
