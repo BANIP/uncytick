bc8674c78e626c4d2b1376427fcb4959260c8ada
BANIP <rnaos90@naver.com>
2018-03-15 23:09:40 +0900

    repair socket

--- server/index.js
+++ server/index.js
@@ -4,30 +4,19 @@
 const express = require("express");
 //const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
+const staticpath = path.resolve(__dirname,"../public");
+
+app.use(express.static(staticpath))
 // [CONFIGURE SOCKET]
 const server = require("http").createServer(app);
 var socketio = require("./socket")(server);
 
-// [CONFIGURE SECURITY-POLICY]
-app.use(helmet.csp({
-    'default-src': ["'self'"],
-    'connect-src': [
-      "'self'" , "blob:",
-      'wss:',
-      'websocket.domain',
-    ],
-    'font-src': ["'self'",'s3.amazonaws.com',"maxcdn.bootstrapcdn.com"],
-    'img-src': ["'self'", 'data:'],
-    'style-src': ["'self'","maxcdn.bootstrapcdn.com",'s3.amazonaws.com',"'unsafe-inline'"],
-    'script-src': ["'self'","'unsafe-inline'","'unsafe-eval'",'blob:'],
-    reportOnly: false,
-    setAllHeaders: false,
-    safari5: false
-  }))
-  
-// [console]
-console.log("uncytick module start")
+server.listen(8080,(req,res) => {
+    console.log("Express server has started on port 8080");
+});
+
 
 module.exports = app;
 
